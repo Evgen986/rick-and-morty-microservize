e91c727dce0fe4e5eaaf4cc122d45548cd088e03
EvgenyMaliutin <evgen986@mail.ru>
2024-01-28 17:23:37 +0300

    initial commit

---
+++ rick-and-morty-web/src/main/java/ru/maliutin/rickandmortyweb/RickAndMortyWebApplication.java
@@ -0,0 +1,13 @@
+package ru.maliutin.rickandmortyweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RickAndMortyWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RickAndMortyWebApplication.class, args);
+    }
+
+}
